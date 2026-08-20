55e57b170cfa865683f20fa4f4937bb5bf0a000b
Abhishek Kumar <abhishekkumarsingh845454@gmail.com>
2026-08-20 20:56:31 +0530

    Distributed elements

---
+++ DistributeElementsIntoTwoArraysI.java
@@ -0,0 +1,34 @@
+// 3069. Distribute Elements Into Two Arrays I
+
+class Solution {
+    public int[] resultArray(int[] nums) {
+        int n = nums.length;
+      
+        int[] firstArray = new int[n];
+        int[] secondArray = new int[n];
+      
+        firstArray[0] = nums[0];
+        secondArray[0] = nums[1];
+      
+        int lastIndexFirst = 0;
+        int lastIndexSecond = 0;
+      
+        for (int currentIndex = 2; currentIndex < n; currentIndex++) {
+            if (firstArray[lastIndexFirst] > secondArray[lastIndexSecond]) {
+                lastIndexFirst++;
+                firstArray[lastIndexFirst] = nums[currentIndex];
+            } else {
+                lastIndexSecond++;
+                secondArray[lastIndexSecond] = nums[currentIndex];
+            }
+        }
+      
+        for (int index = 0; index <= lastIndexSecond; index++) {
+            lastIndexFirst++;
+            firstArray[lastIndexFirst] = secondArray[index];
+        }
+      
+        // Return the merged result
+        return firstArray;
+    }
+}
