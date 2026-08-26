e47685ea2055fc4634dcdae2dbfa52841f1a8cb6
Abhishek Kumar <abhishekkumarsingh845454@gmail.com>
2026-08-26 22:43:05 +0530

    lexographically smallest

---
+++ LexicographicallySmallest.java
@@ -0,0 +1,30 @@
+// 2904. Shortest and Lexicographically Smallest Beautiful String
+
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        int stringLength = s.length();
+        String result = "";
+      
+        for (int startIndex = 0; startIndex < stringLength; ++startIndex) {
+            for (int endIndex = startIndex + k; endIndex <= stringLength; ++endIndex) {
+                // Extract the current substring
+                String currentSubstring = s.substring(startIndex, endIndex);
+              
+                int onesCount = 0;
+                for (char character : currentSubstring.toCharArray()) {
+                    onesCount += character - '0'; 
+                }
+              
+                if (onesCount == k && 
+                    (result.isEmpty() ||                              
+                     endIndex - startIndex < result.length() ||    
+                     (endIndex - startIndex == result.length() &&     
+                      currentSubstring.compareTo(result) < 0))) {     
+                    result = currentSubstring;
+                }
+            }
+        }
+      
+        return result;
+    }
+}
